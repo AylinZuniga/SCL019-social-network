2c119d1b03db6863dbc8fc74b8327cf50401ff63
FranciscaValdes <fran_valdes_m@live.com>
2022-02-22 10:18:56 -0300

    commit antes del pull

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,19 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
  https://firebase.google.com/docs/web/setup#available-libraries
-
-
- import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
-
-
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
-
-
-
-
-
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
