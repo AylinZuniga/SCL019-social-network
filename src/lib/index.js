9a8da32cebb8ce914fd805c3570c403f7daa35b5
amapolamory <amapola.rebeca.mory@gmail.com>
2022-02-24 11:06:04 -0300

    cambio firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,19 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
-
-
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
  https://firebase.google.com/docs/web/setup#available-libraries
-
-
-
-
-
-
-
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
