9125de4b27d2448da73a71e1b87be106f6f091f0
amapolamory <amapola.rebeca.mory@gmail.com>
2022-03-14 16:53:58 -0300

    cambio css

--- src/templates/wall.js
+++ src/templates/wall.js
@@ -1,16 +1,20 @@
 import { addPost } from '../lib/index.js';
 export const wall = () => {
-    const wallPage = document.createElement('main');
-    wallPage.className = 'wall-page';
-    wallPage.id = 'wallPage';
-    const wallPageView = `
-       <header class="wallHeader">
+  const wallPage = document.createElement('main');
+  wallPage.className = 'wall-page';
+  wallPage.id = 'wallPage';
+  const wallPageView = `
+     
+       <header class="title-menu">
 
-        <div class="wallHeaderContainer">
-            <h1 class="wallTitle">Together</h1>
-            <a href="#/"> <img src="./imagenes/exit.png" class="exitIcon"></a>
-        </div>
-
+          <div class="titleContainer">
+            <h1 class="title">Together</h1>
+          </div>
+     
+          <div class="homeContainer">
+            <a class="iconContainer" href="#/"> <img src="./imagenes/exit.png" class="exitIcon"></a>
+          </div>
+     
        </header>
 
        <div class="postAndWall">
@@ -21,11 +25,11 @@ export const wall = () => {
                  <a href="#/"> <img src="./imagenes/profile.png" class="profilePic"></a>  
                 </section>
         
-                <div class="publishContainer">   
+                <form class="publishContainer">   
                     <p class="questionP">¿Qué estas pensando?</p>
                     <textarea name="postText" class="postText" id="postText"></textarea>
                     <button class="publish" id="publish">Publicar</button>
-                </div>
+                </form>
 
             </div>
 
@@ -37,38 +41,38 @@ export const wall = () => {
         </div>
     
     `;
-    // wallPage.querySelector('#exitIcon').addEventListener('click', () => {
-    //     logoutButton();
-    //   });
-    // Producto en desarrollo
-    // Evento para guardar datos de post
-    const printPage = document.getElementById('root');
-    printPage.innerHTML = wallPageView;
-    
-
-
-    const newPost = printPage.querySelector('#publish');
-    newPost.addEventListener('click', () => {
-        const inputPost = document.getElementById('postText').value;
-        if (inputPost === '') {
-            alert('Complete los campos vacios');
-        } else {
-            addPost(inputPost);
-        }
-        document.getElementById('postText').value = '';
-    });
+  // wallPage.querySelector('#exitIcon').addEventListener('click', () => {
+  //     logoutButton();
+  //   });
+  // Producto en desarrollo
+  // Evento para guardar datos de post
+  const printPage = document.getElementById('root');
+  printPage.innerHTML = wallPageView;
 
 
 
-    
+  const newPost = printPage.querySelector('#publish');
+  newPost.addEventListener('submit', (e) => {
+    e.preventDefault
+    const inputPost = document.getElementById('postText').value;
 
-    // Evento para cerrar sesión
-    printPage.querySelector('.exitIcon').addEventListener('click', () => {
+    addPost(firebaseObject.db, description);
+    console.log(addPost)
+    console.log(newPost)
+    publishContainer.reset;
+  });
+
+
+
+
+
+  // Evento para cerrar sesión
+  printPage.querySelector('.exitIcon').addEventListener('click', () => {
     logOut();
   });
 
-    // Evento para guardar post
-    //probandoo
-    return printPage;
+  // Evento para guardar post
+  //probandoo
+  return printPage;
 
 };
